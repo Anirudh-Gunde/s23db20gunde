bcda0313d27a7c18ea5dadb353013911d19e9b81
Anirudh Gunde <anirudh.gunde1@gmail.com>
2023-04-21 11:53:10 -0500

    added changes

--- controllers/Zipper.js
+++ controllers/Zipper.js
@@ -130,3 +130,16 @@ exports.zipper_update_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
     };
+// Handle a delete one view with id from query
+exports.zipper_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await zipper.findById(req.query.id)
+    res.render('zipperdelete', { title: 'zipper Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
